55d5c7507c1ab184fce33b0ade69bbbf32c0fb1b
Kathleen Forgiarini da Silva <82175930+kathleenforgiarini@users.noreply.github.com>
2023-09-11 10:08:13 -0400

    using pow and function
    Created a function to round a number

--- BMI-Calculator.playground/Contents.swift
+++ BMI-Calculator.playground/Contents.swift
@@ -5,12 +5,19 @@ let lastName: [String] = ["Carvalho", "Zukemberg", "Smith", "Olivier", "Francois
 let height: [Int] = [174, 178, 166, 182, 177]  // in centimeters
 let weight: [Double] = [82.5, 78.2, 120.0, 71.2, 92.9]  // in kg
 
+func roundTo(number: Double, decimals: Double) -> Double  {
+    let baseNumber = pow(10.0,decimals)
+    let roundedNumber = round(number * baseNumber) / baseNumber
+    
+    return roundedNumber
+}
+
 for i in 0..<firstName.count {
     
   let heightInM = Double(height[i]) / 100
-  let bmi = weight[i] / (heightInM * heightInM)
+  let bmi = weight[i] / pow(heightInM,2)
     
-  let roundBmi = round(bmi * 10) / 10
+  let roundBmi = roundTo(number: bmi, decimals: 1)
     
   var classification = ""
     
